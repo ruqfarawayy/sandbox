c702b048b6048a7fdd8f6edfb7191761723c376d
Faruq Saeful <faruqsaeful@gmail.com>
2023-07-08 14:26:02 +0700

    still dev try

--- next.config.js
+++ next.config.js
@@ -1,14 +1,16 @@
 const nextConfig = {
-  publicRuntimeConfig: {
-    AppName: "SANDBOX",
-  },
-  eslint: {
-    dirs: ["."],
-  },
-  poweredByHeader: false,
-  trailingSlash: true,
-  transpilePackages: ["antd"],
-  reactStrictMode: false,
-};
+	publicRuntimeConfig: {
+		AppName: 'SANDBOX'
+	},
+	eslint: {
+		dirs: ['.']
+	},
+	poweredByHeader: false,
+	trailingSlash: true,
+	transpilePackages: ['antd'],
+	reactStrictMode: false,
+	output: 'export',
+  distDir: 'dist'
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
